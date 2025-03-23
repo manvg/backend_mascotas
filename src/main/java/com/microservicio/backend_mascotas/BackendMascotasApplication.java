6e3728eb295d3c754fcf67157f789a61a04c6d1d
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-03-23 16:50:28 -0300

    Creación microservicio gestión datos de mascotas veterinaria

---
+++ src/main/java/com/microservicio/backend_mascotas/BackendMascotasApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.backend_mascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMascotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservicio/backend_mascotas/BackendMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.backend_mascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
