326f83773f236597d6d20dfb3da744d4990c73d2
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-03-23 18:15:49 -0300

    Carga de datos en repositorio en memoria

--- src/main/java/com/microservicio/backend_mascotas/repository/MascotaRepository.java
+++ src/main/java/com/microservicio/backend_mascotas/repository/MascotaRepository.java
@@ -12,6 +12,63 @@ public class MascotaRepository {
     private final List<MascotaDto> mascotas = new ArrayList<>();
     private final AtomicLong idGenerator = new AtomicLong();
 
+    public MascotaRepository() {
+        MascotaDto m1 = new MascotaDto();
+    m1.setIdMascota(idGenerator.incrementAndGet());
+    m1.setNombre("Luna");
+    m1.setEspecie("Perro");
+    m1.setRaza("Labrador Retriever");
+    m1.setSexo("F");
+    m1.setPesoKg(23.5);
+    m1.setObservaciones("Muy activa, necesita espacio");
+    m1.setRutCliente("12345678-9");
+    mascotas.add(m1);
+
+    MascotaDto m2 = new MascotaDto();
+    m2.setIdMascota(idGenerator.incrementAndGet());
+    m2.setNombre("Max");
+    m2.setEspecie("Gato");
+    m2.setRaza("Siames");
+    m2.setSexo("M");
+    m2.setPesoKg(4.2);
+    m2.setObservaciones("Tranquilo y dormilón");
+    m2.setRutCliente("98765432-1");
+    mascotas.add(m2);
+
+    MascotaDto m3 = new MascotaDto();
+    m3.setIdMascota(idGenerator.incrementAndGet());
+    m3.setNombre("Coco");
+    m3.setEspecie("Perro");
+    m3.setRaza("Poodle");
+    m3.setSexo("M");
+    m3.setPesoKg(8.0);
+    m3.setObservaciones("Alergias estacionales");
+    m3.setRutCliente("24681357-0");
+    mascotas.add(m3);
+
+    MascotaDto m4 = new MascotaDto();
+    m4.setIdMascota(idGenerator.incrementAndGet());
+    m4.setNombre("Maya");
+    m4.setEspecie("Conejo");
+    m4.setRaza("Enano holandés");
+    m4.setSexo("F");
+    m4.setPesoKg(1.3);
+    m4.setObservaciones("Dieta estricta");
+    m4.setRutCliente("13579246-8");
+    mascotas.add(m4);
+
+    MascotaDto m5 = new MascotaDto();
+    m5.setIdMascota(idGenerator.incrementAndGet());
+    m5.setNombre("Rocky");
+    m5.setEspecie("Perro");
+    m5.setRaza("Bulldog Francés");
+    m5.setSexo("M");
+    m5.setPesoKg(12.4);
+    m5.setObservaciones("Le teme a los truenos");
+    m5.setRutCliente("10293847-6");
+    mascotas.add(m5);
+    }
+
     public List<MascotaDto> findAll() {
         return mascotas;
     }
